6815ec841e55bccb021c21236cdaa9a003396e32
Wesam Ali Alghjamia <wgjamia@gmail.com>
2021-10-11 22:38:27 +0200

    Write the comment of the Method in the Interface

--- oopVirtualKeyProject/src/ly/oopVirtualKeyProject/welcomescreen/Screens.java
+++ oopVirtualKeyProject/src/ly/oopVirtualKeyProject/welcomescreen/Screens.java
@@ -2,10 +2,11 @@ package ly.oopVirtualKeyProject.welcomescreen;
 
 public interface Screens {
 	
+	// Method of Show the Welcome Screen
 	public void ShowSecreen();
-
+	// Method for Navigation Option of Main Menu
     public void NavigateOption(int option);
-	    
+	 // Method for Get User Input    
     public void GetUserInput();
 
 }
