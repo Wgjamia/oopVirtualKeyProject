25a567c75d082d665400871e63bcd338219037ab
Wesam Ali Alghjamia <wgjamia@gmail.com>
2021-10-11 22:33:04 +0200

    Create Interface for models of screens

---
+++ oopVirtualKeyProject/src/ly/oopVirtualKeyProject/welcomescreen/Screens.java
@@ -0,0 +1,11 @@
+package ly.oopVirtualKeyProject.welcomescreen;
+
+public interface Screens {
+	
+	public void ShowSecreen();
+
+    public void NavigateOption(int option);
+	    
+    public void GetUserInput();
+
+}
