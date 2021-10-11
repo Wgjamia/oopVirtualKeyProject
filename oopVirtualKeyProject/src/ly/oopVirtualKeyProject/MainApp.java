609c0a0383465e2500adad59448c1a56f816a93d
Wesam Ali Alghjamia <wgjamia@gmail.com>
2021-10-11 23:22:58 +0200

    Create class of Main Project Virtual Key.

---
+++ oopVirtualKeyProject/src/ly/oopVirtualKeyProject/MainApp.java
@@ -0,0 +1,17 @@
+package ly.oopVirtualKeyProject;
+
+import ly.oopVirtualKeyProject.welcomescreen.WelcomeScreen;
+
+public class MainApp {
+
+	// Main Method of the project Virtual Key
+	public static void main(String[] args) {
+		// Create object of welcome Screen.
+		WelcomeScreen welcome = new WelcomeScreen();
+		// call method print of Main Menu from Class WelcomeScreen
+    	welcome.printSoftD();
+    	// call method get User Input of Main Menu from Class WelcomeScreen
+    	welcome.GetUserInput();
+	}
+
+}
