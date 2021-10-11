5cba34bc46b785440d30b4d482725ebf4c7e874d
Wesam Ali Alghjamia <wgjamia@gmail.com>
2021-10-11 23:18:17 +0200

    Create Class of Welcome Screen

---
+++ oopVirtualKeyProject/src/ly/oopVirtualKeyProject/welcomescreen/WelcomeScreen.java
@@ -0,0 +1,109 @@
+package ly.oopVirtualKeyProject.welcomescreen;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import ly.oopVirtualKeyProject.controler.DirectoryControler;
+import ly.oopVirtualKeyProject.controler.ScreenControler;
+
+
+public class WelcomeScreen implements Screens 
+{
+
+	// Create Variable of Welcome Main Menu.
+    private String welcomeMainMenu = " || ========> Welcome to the project Virtual Key <======== ||";
+    
+    // Create Variable of Owner of Project.
+    private String developerName = "================>> Developer: Wesam Alghjamia <<================";
+
+    // Create Object of options as ArrayList.
+    private ArrayList<String> options = new ArrayList<>();
+	
+    // Create Constructor of Welcome Screen and initial options values
+    public WelcomeScreen() {
+    	  options.add("1. Show Files");
+          options.add("2. Show File Options Menu");
+          options.add("3. Quit");
+    }
+    
+    // Create method of print initial of Welcome Main Menu.
+    public void printSoftD() {
+    	System.out.println(welcomeMainMenu);
+        System.out.println(developerName);
+        System.out.println("\n");
+        System.out.println("<<=============================================================================>>");
+        System.out.println("\n");
+        ShowSecreen();
+    }
+    
+    // Override the method of Show Screen.
+	@Override
+	public void ShowSecreen() {
+		System.out.println("Main Menu");
+        for (String s : options)  {
+            System.out.println(s);
+        }
+	}
+	// Override the method of Navigate Option.
+	@Override
+	public void NavigateOption(int option) {
+		  switch(option) {
+
+          case 1: // Show Files in Directory
+              this.ShowFiles();
+              
+              this.ShowSecreen();
+              
+              break;
+              
+          case 2: // Show File Options menu
+        	  ScreenControler.setCurrentScreen(ScreenControler.FileOptionsScreen);
+        	  ScreenControler.getCurrentScreen().ShowSecreen();
+        	  ScreenControler.getCurrentScreen().GetUserInput();
+              
+              this.ShowSecreen();
+              
+              break;
+              
+          default:
+              System.out.println("Invalid Option");
+              break;
+      }
+	}
+	// Override the method Get User Input of option.
+	@Override
+	public void GetUserInput() {
+		   int selectedOption  = 0;
+	        while ((selectedOption = this.getOption()) != 3) {
+	            this.NavigateOption(selectedOption);
+	        }
+	}
+	
+	// Create method of show files.
+	  public void ShowFiles() {
+
+
+	        System.out.println("List of Files: ");
+	    	DirectoryControler.PrintFiles();
+
+	    }
+	    
+	  // Create method to get Option.
+	  
+	    private int getOption() {
+	        Scanner in = new Scanner(System.in);
+
+	        int returnOption = 0;
+	        try {
+	            returnOption = in.nextInt();
+	        }
+	        catch (InputMismatchException ex) {
+
+	        }
+	        return returnOption;
+
+	    }
+	
+
+}
